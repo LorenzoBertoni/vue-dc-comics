8db789b3cbcf50b3226abc7ed903fdeb7fcec188
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-08-24 15:05:48 +0200

    add: componente appheader + contenuto header componente appmain + contenuto main componente appfooter style: regole generali + mixin per centratura flex

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,15 +1,73 @@
 <template>
-    <div>
-        
-    </div>
+    <main>
+        <section id="main-content">
+            <div>
+                <h1>Content goes here</h1>
+            </div>
+        </section>
+
+        <section id="products">
+            <!-- <ul>
+                <li v-for="(product, index) in products" :key="index">
+                    <img :src="product.logo" alt="">
+                    <span>{{product.text}}</span>
+                </li>
+            </ul> -->
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
-    name: 'AppContent'
+    name: 'AppContent',
+    /*
+    data() {
+        return {
+            products: [
+                {
+                    logo: 'buy-comics-digital-comics.png',
+                    text: 'Digital Comics'
+                },
+                {
+                    logo: 'comics-merchandise.png',
+                    text: 'DC merchandise'
+                },
+                {
+                    logo: 'comics-subscription.png',
+                    text: 'Subscription'
+                },
+                {
+                    logo: 'comics-digital-comics.png',
+                    text: 'Comic shop locator'
+                },
+                {
+                    logo: 'comics-digital-comics.png',
+                    text: 'DC power visa'
+                },
+            ]
+        }
+    }
+    */
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
+@import '../styles/variables';
+@import '../styles/mixins.scss';
+
+    #main-content {
+        background-color: $content_sect_color;
+        padding: 3rem 20rem;
+
+        h1 {
+            color: #fff;
+        }
+    }
+
+    #products {
+        height: 200px;
+        background-color: $brand_color;
+        @include flex('both');
+    }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,114 @@
 <template>
     <header>
+        <a href="#">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </a>
 
+        <ul>
+            <li v-for="(list, index) in lists" :key="index" :class="list.isActive?'active':''">
+                <a :href="list.url">{{list.text}}</a>
+            </li>
+        </ul>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            lists: [
+                    {
+                        text: 'Characters',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Comics',
+                        isActive: true,
+                        url: '#'
+                    },
+                    {
+                        text: 'Movies',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'TV',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Games',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Collectibles',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Videos',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Fans',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'News',
+                        isActive: false,
+                        url: '#'
+                    },
+                    {
+                        text: 'Shop',
+                        isActive: false,
+                        url: '#'
+                    },
+                ]
+            }
+        }
+        
 }
 </script>
 
 <style scoped lang="scss">
+
+@import '../styles/variables.scss';
+@import '../styles/mixins.scss';
+
     header {
-        height: 150px;
-        background-color: lightblue;
+        @include flex('vertical');
+        justify-content: space-around;
+        padding: 1rem 0;
+
+        ul {
+            li {
+                display: inline-block;
+                margin: 0 15px;
+                line-height: calc(107px - 1rem);
+
+                &.active {
+                    border-bottom: 5px solid $brand_color;
+
+                    a {
+                        color: $brand_color;
+                    }
+                }
+                //a figlio di li
+                    a {
+                        color: #464646;
+                        text-decoration: none;
+                        font-weight: bold;
+
+                    &:hover {
+                        color: $brand_color;
+                    }
+                }
+            }
+        }
     }
 </style>
